dbe8da4e04c80999d5aec5a6c1715c62faa1ede2
Gregwar <g.passault@gmail.com>
2022-11-16 12:24:26 +0100

    Adding support frame in legged robot

--- src/placo/model/legged_robot.h
+++ src/placo/model/legged_robot.h
@@ -8,5 +8,15 @@ class LeggedRobot : public MobileRobot
 {
 public:
   LeggedRobot(std::string model_directory = "robot/");
+
+  /**
+   * @brief Updates which frame should be the current support
+   */
+  void update_support(MobileRobot::FrameIndex new_support_frame);
+  void update_support(const std::string &frame);
+
+  // We suppose we have one support frame and associated transformation
+  MobileRobot::FrameIndex support_frame;
+  Eigen::Affine3d T_world_support;
 };
 }  // namespace placo
