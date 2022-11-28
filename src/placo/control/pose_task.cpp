1c14efd2396b308193a9586bc502a9dc404c4dce
Gregwar <g.passault@gmail.com>
2022-11-28 12:59:34 +0100

    Using Jlog6 in the body jacobian

--- src/placo/control/pose_task.cpp
+++ src/placo/control/pose_task.cpp
@@ -11,17 +11,16 @@ PoseTask::PoseTask(RobotWrapper::FrameIndex frame_index, Eigen::Affine3d T_world
 void PoseTask::update()
 {
   auto T_world_frame_current = solver->robot->get_T_world_frame(frame_index);
-
-  auto M = (T_world_frame * T_world_frame_current.inverse()).matrix();
+  auto M = (T_world_frame_current.inverse() * T_world_frame).matrix();
 
   // (T_frame T_current^{-1}) T_current = T_frame
   // |-----------------------|
-  //            | This part is the world error that "correct" the transformatio
+  //            | This part is the world error that "correct" the transformation
   Eigen::VectorXd error = pinocchio::log6(M).toVector();
   Eigen::Matrix<double, 6, 6> Jlog;
   pinocchio::Jlog6(pinocchio::SE3(M), Jlog);
 
-  A = Jlog * solver->robot->frame_jacobian(frame_index, pinocchio::WORLD);
+  A = Jlog * solver->robot->frame_jacobian(frame_index, pinocchio::LOCAL);
   b = error;
 }
 
