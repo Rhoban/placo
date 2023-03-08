ac92249d93d8c0515ac467e38355c13834c32a21
Grégoire Passault <g.passault@gmail.com>
2023-03-08 23:41:22 +0100

    Changing the way SwingFoot works (same result)

--- src/placo/planning/swing_foot.cpp
+++ src/placo/planning/swing_foot.cpp
@@ -3,33 +3,26 @@
 
 namespace placo
 {
-struct FactorCubicHermiteCurve
+Eigen::VectorXd position_coefficients(double t)
 {
-  FactorCubicHermiteCurve(Eigen::Vector3d p0, Eigen::Vector3d n0, Eigen::Vector3d p1, Eigen::Vector3d n1)
-    : p0(p0), n0(n0), p1(p1), n1(n1)
-  {
-  }
+  double t_2 = t * t;
+  double t_3 = t_2 * t;
 
-  Eigen::Vector3d H_lambda(double t)
-  {
-    return t * (1 + t * (t - 2)) * n0;
-  }
+  // ax^3 + bx^2 + cx + d
+  Eigen::VectorXd v(4);
+  v << t_3, t_2, t, 1;
+  return v;
+}
 
-  Eigen::Vector3d H_mu(double t)
-  {
-    return t * t * (t - 1) * n1;
-  };
+Eigen::VectorXd velocity_coefficients(double t)
+{
+  double t_2 = t * t;
 
-  Eigen::Vector3d H_cst(double t)
-  {
-    return p0 + t * t * (3 - 2 * t) * (p1 - p0);
-  };
-
-  Eigen::Vector3d p0;
-  Eigen::Vector3d n0;
-  Eigen::Vector3d p1;
-  Eigen::Vector3d n1;
-};
+  // 3ax^2 + 2bx + c
+  Eigen::VectorXd v(4);
+  v << 3 * t_2, 2 * t, 1, 0;
+  return v;
+}
 
 SwingFoot::Trajectory SwingFoot::make_trajectory(double t_start, double t_end, double height, Eigen::Vector3d start,
                                                  Eigen::Vector3d target)
@@ -38,80 +31,54 @@ SwingFoot::Trajectory SwingFoot::make_trajectory(double t_start, double t_end, d
   trajectory.t_start = t_start;
   trajectory.t_end = t_end;
 
-  Eigen::Vector3d n0(0., 0., 1.);
-  Eigen::Vector3d n1(0., 0., 1.);
+  // Constraining the starting and ending position, and the starting
+  // and ending velocities.
+  Eigen::MatrixXd A(4, 4);
+  A.setZero();
+  A << position_coefficients(t_start),  //
+      position_coefficients(t_end),     //
+      velocity_coefficients(t_start),   //
+      velocity_coefficients(t_end);
+  A = A.transpose().inverse();
 
-  double t_a = 1 / 4.;
-  double t_b = 3 / 4.;
+  Eigen::VectorXd x(4);
+  x << start.x(), target.x(), 0., 0.;
+  Eigen::VectorXd abcd_x = A * x;
 
-  FactorCubicHermiteCurve factors(start, n0, target, n1);
+  Eigen::VectorXd y(4);
+  y << start.y(), target.y(), 0., 0.;
+  Eigen::VectorXd abcd_y = A * y;
 
-  double a0 = factors.H_lambda(t_a).dot(n0);
-  double b0 = factors.H_mu(t_a).dot(n0);
-  double c0 = (factors.H_cst(t_a) - start).dot(n0);
+  // Constraining position when starting, 1/4, 3/4 and ending
+  Eigen::MatrixXd B(4, 4);
+  B << position_coefficients(t_start),                                 //
+      position_coefficients(t_start + (1. / 4.) * (t_end - t_start)),  //
+      position_coefficients(t_start + (3. / 4.) * (t_end - t_start)),  //
+      position_coefficients(t_end);                                    //
+  B = B.transpose().inverse();
 
-  double a1 = factors.H_lambda(t_b).dot(n1);
-  double b1 = factors.H_mu(t_b).dot(n1);
-  double c1 = (factors.H_cst(t_b) - start).dot(n1);
+  Eigen::VectorXd z(4);
+  z << start.z(), height, height, target.z();
+  Eigen::VectorXd abcd_z = B * z;
 
-  Eigen::MatrixXd CE(0, 0);
-  Eigen::VectorXd ce(0);
-
-  Eigen::MatrixXd P(2, 2);
-  P.setIdentity();
-  Eigen::VectorXd q(2);
-  q.setZero();
-
-  Eigen::MatrixXd G(2, 2);
-  G << a0, b0,  //
-      a1, b1;
-  Eigen::VectorXd h(2);
-  h << c0 - height, c1 - height;
-
-  Eigen::VectorXd x(2);
-
-  Eigen::VectorXi activeSet;
-  size_t activeSetSize;
-  double result = eiquadprog::solvers::solve_quadprog(P, q, CE, ce, G.transpose(), h, x, activeSet, activeSetSize);
-
-  if (result == std::numeric_limits<double>::infinity())
-  {
-    throw std::runtime_error("SwingFoot: Infeasible QP (check your equality and inequality constraints)");
-  }
-
-  trajectory.compute_abcd(start, n0 * x[0], target, n1 * x[1]);
+  trajectory.a = Eigen::Vector3d(abcd_x[0], abcd_y[0], abcd_z[0]);
+  trajectory.b = Eigen::Vector3d(abcd_x[1], abcd_y[1], abcd_z[1]);
+  trajectory.c = Eigen::Vector3d(abcd_x[2], abcd_y[2], abcd_z[2]);
+  trajectory.d = Eigen::Vector3d(abcd_x[3], abcd_y[3], abcd_z[3]);
 
   return trajectory;
 }
 
-void SwingFoot::Trajectory::compute_abcd(Eigen::Vector3d p0, Eigen::Vector3d m0, Eigen::Vector3d p1, Eigen::Vector3d m1)
+Eigen::Vector3d SwingFoot::Trajectory::pos(double t)
 {
-  a = 2 * p0 - 2 * p1 + m0 + m1;
-  b = -3 * p0 + 3 * p1 - 2 * m0 - m1;
-  c = m0;
-  d = p0;
-}
-
-Eigen::Vector3d SwingFoot::Trajectory::pos(double t_)
-{
-  double t = (t_ - t_start) / (t_end - t_start);
-  if (t > 1)
-  {
-    t = 1;
-  }
   double t_2 = t * t;
   double t_3 = t_2 * t;
 
   return a * t_3 + b * t_2 + c * t + d;
 }
 
-Eigen::Vector3d SwingFoot::Trajectory::vel(double t_)
+Eigen::Vector3d SwingFoot::Trajectory::vel(double t)
 {
-  double t = (t_ - t_start) / (t_end - t_start);
-  if (t > 1)
-  {
-    t = 1;
-  }
   double t_2 = t * t;
 
   return (3 * a * t_2 + 2 * b * t + c) / (t_end - t_start);
--- src/placo/planning/swing_foot.h
+++ src/placo/planning/swing_foot.h
@@ -17,8 +17,6 @@ public:
     Eigen::Vector3d pos(double t);
     Eigen::Vector3d vel(double t);
 
-    void compute_abcd(Eigen::Vector3d p0, Eigen::Vector3d m0, Eigen::Vector3d p1, Eigen::Vector3d m1);
-
     // Computed polynom (ax^3 + bx^2 + cx + d)
     Eigen::Vector3d a, b, c, d;
 
