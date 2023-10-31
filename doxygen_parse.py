dcd271300a95706c1015c4511a7eaf51632af12d
Grégoire Passault <g.passault@gmail.com>
2023-10-31 23:58:50 +0100

    Doxygen parse and stubs

---
+++ doxygen_parse.py
@@ -0,0 +1,128 @@
+import glob
+import xml.etree.ElementTree as ET
+
+# Mapping member definitions (id) to all informations
+member_definitions: dict = {}
+
+# Compound members
+compound_members: dict = {}
+
+# Mapping compound id to name
+rewrite_types: dict = {
+    "std::string": "str",
+    "double": "float",
+    "int": "int",
+    "bool": "bool",
+    "Eigen::MatrixXd": "np.ndarray",
+    "Eigen::VectorXd": "np.ndarray",
+    "Eigen::Matrix3d": "np.ndarray",
+    "Eigen::Vector3d": "np.ndarray",
+    "Eigen::Matrix2d": "np.ndarray",
+    "Eigen::Vector2d": "np.ndarray",
+    "void": "None",
+}
+
+
+def resolve_type(node):
+    type_node = node.find("type")
+    if type_node.find("ref") is not None:
+        return type_node.find("ref").attrib["refid"]
+    else:
+        return type_node.text
+
+
+def parse_compound(compounddef_node: ET.Element):
+    global member_definitions
+    name = compounddef_node.find("compoundname").text
+    id = compounddef_node.attrib["id"]
+    rewrite_types[id] = name
+
+    # Searching for member definitions
+    for member in compounddef_node.findall("sectiondef/memberdef"):
+        kind = member.attrib["kind"]
+        id = member.attrib["id"]
+
+        if kind in ["function", "variable", "enum"]:
+
+            member_definitions[id] = {
+                "kind": kind,
+                "name": member.find("name").text,
+                "params": [],
+            }
+            member_definitions[id]["type"] = resolve_type(member)
+
+            for param in member.findall("param"):
+                param_type = resolve_type(param)
+                param_name = None
+                if param.find("declname") is not None:
+                    param_name = param.find("declname").text
+                if param.find("defname") is not None:
+                    param_name = param.find("defname").text
+                def_val = param.find("defval").text if param.find("defval") is not None else None
+                member_definitions[id]["params"].append({"type": param_type, "name": param_name, "default": def_val})
+
+            # Brief description
+            brief = member.find("briefdescription/para")
+            if brief is not None:
+                member_definitions[id]["brief"] = brief.text
+
+            # Detailed description
+            member_definitions[id]["detailed"] = []
+            for entry in member.findall("detaileddescription/para/parameterlist/parameteritem"):
+                param_name = entry.find("parameternamelist/parametername").text
+                param_desc = entry.find("parameterdescription/para").text
+                member_definitions[id]["detailed"].append({"name": param_name, "desc": param_desc})
+            
+            # Return type
+            return_type = member.find("detaileddescription/para/simplesect[@kind='return']")
+            if return_type:
+                member_definitions[id]["returns"] = return_type.find('para').text
+        elif kind == "typedef":
+            rewrite_types[id] = resolve_type(member)
+
+    # Listinf all members
+    compound_members[name] = []
+    for member in compounddef_node.findall("listofallmembers/member"):
+        compound_members[name].append(member.attrib["refid"])
+
+
+def parse_xml(xml_file: str):
+    with open(xml_file, "r") as f:
+        xml_content = f.read()
+        xml_content = ET.fromstring(xml_content)
+
+        # Searching for compounddef nodes (classs & struct)
+        for compounddef_node in xml_content.findall("compounddef"):
+            parse_compound(compounddef_node)
+
+
+def parse_directory(directory):
+    global member_definitions, compound_members
+    doxygen_xml_files = glob.glob(f"{directory}/xml/*.xml")
+
+    for xml_file in doxygen_xml_files:
+        parse_xml(xml_file)
+
+    # Resolving types
+    for member in member_definitions:
+        member_definition = member_definitions[member]
+        if member_definition["type"] in rewrite_types:
+            member_definition["type"] = rewrite_types[member_definition["type"]]
+
+        for param in member_definition["params"]:
+            if param["type"] in rewrite_types:
+                param["type"] = rewrite_types[param["type"]]
+
+    # Resolving compound members
+    for name in compound_members:
+        temp = compound_members[name]
+        compound_members[name] = {}
+        for id in temp:
+            if id in member_definitions:
+                compound_members[name][member_definitions[id]["name"]] = member_definitions[id]
+
+def get_members(name: str):
+    if name not in compound_members:
+        return None
+    
+    return compound_members[name]
--- stubs.py
+++ stubs.py
@@ -3,10 +3,46 @@ import re
 import inspect
 import placo
 import os
+import glob
+from doxygen_parse import parse_directory, get_members, rewrite_types
+
+# Current script directory:
+repo_directory = os.path.dirname(os.path.realpath(__file__))
+
+# Ensure Doxygen is run
+os.system(f"cd {repo_directory} && doxygen 1>&2")
+
+# Read the Doxygen XML file
+parse_directory(repo_directory)
+
+# Building registry and reverse registry for class names
+cxx_registry = placo.get_classes_registry()
+py_registry = {}
+for entry in cxx_registry:
+    py_registry[cxx_registry[entry]] = entry
+
+
+def get_member(class_name: str, member_name: str):
+    if class_name in py_registry:
+        cxx_name = py_registry[class_name]
+        members = get_members(cxx_name)
+        if members is not None and member_name in members:
+            return members[member_name]
+
+    return None
+
+
+def cxx_type_to_py(cxx_type: str) -> str:
+    if cxx_type in cxx_registry:
+        return cxx_registry[cxx_type]
+    elif cxx_type in rewrite_types:
+        return rewrite_types[cxx_type]
+    else:
+        return cxx_type.replace(":", "_")
 
 
 def parse_doc(name: str, doc: str) -> dict:
-    definition = {"name": name, "args": [], "returns": "Any"}
+    definition = {"name": name, "args": [], "returns": "any"}
 
     prototype = doc.strip().split("\n")[0].strip()
 
@@ -30,21 +66,78 @@ def print_def(name: str, doc: str, prefix: str = ""):
     definition = parse_doc(name, doc)
     str_definition = prefix
     str_definition += f"def {definition['name']}("
-    str_definition += ",".join([f"{arg_name}: {arg_type}" for arg_type,
-                               arg_name in definition["args"]])
+    str_definition += ",".join([f"{arg_name}: {arg_type}" for arg_type, arg_name in definition["args"]])
     str_definition += f") -> {definition['returns']}: ..."
     print(str_definition)
 
 
+def print_class_member(class_name: str, member_name: str):
+    member = get_member(class_name, member_name)
+
+    if member is not None:
+        py_type = cxx_type_to_py(member["type"])
+        print(f"  {member_name}: {py_type}")
+        if "brief" in member:
+            print(f"  \"\"\"{member['brief']}\"\"\"")
+    else:
+        print(f"  {member_name}: any")
+
+
+def print_class_method(class_name: str, method_name: str, doc: str, prefix: str = ""):
+    if method_name == "__init__":
+        member = get_member(class_name, class_name)
+    else:
+        member = get_member(class_name, method_name)
+
+    if member is not None:
+        # Method name
+        str_definition = f"  def {method_name}("
+
+        # Method arguments
+        str_definition += ",".join([f"self: {class_name}"] + [f"{arg['name']}: {cxx_type_to_py(arg['type'])}" for arg in member["params"]])
+
+        # Return type
+        str_definition += f")"
+        if member["type"]:
+            py_type = cxx_type_to_py(member["type"])
+            str_definition += f" -> {py_type}"
+        str_definition += ":\n"
+
+        # Brief
+        brief_str = ""
+        if "brief" in member:
+            brief_str = member["brief"] + "\n"
+
+        if "detailed" in member:
+            for param in member["detailed"]:
+                brief_str += f"\n    :param {param['name']}: {param['desc']}"
+
+        if "returns" in member:
+            brief_str += f"\n    :return: {member['returns']}"
+
+        if brief_str:
+            str_definition += f'    """\n    {brief_str}\n    """\n'
+        str_definition += "    ...\n"
+
+        print(str_definition)
+        # py_type = cxx_type_to_py(member['type'])
+        # print(f"  def {method_name}() -> {py_type}: ...")
+    else:
+        print_def(method_name, doc, prefix)
+
+
+print("import numpy as np")
+
 for name, object in inspect.getmembers(placo):
     if isinstance(object, type):
-        print(f"class {object.__name__}:")
+        class_name = object.__name__
+        print(f"class {class_name}:")
         for _name, _object in inspect.getmembers(object):
             if not _name.startswith("_") or _name == "__init__":
                 if callable(_object):
-                    print_def(_name, _object.__doc__, "  ")
+                    print_class_method(class_name, _name, _object.__doc__, "  ")
                 else:
-                    print(f"  {_name}: Any")
+                    print_class_member(class_name, _name)
         print("")
     elif callable(object):
         print_def(name, object.__doc__)
