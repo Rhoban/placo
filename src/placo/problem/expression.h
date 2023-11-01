a0a65dc86031271dbed2e434167d03c2af337127
Grégoire Passault <g.passault@gmail.com>
2023-11-01 16:38:43 +0100

    Documenting expressions

--- src/placo/problem/expression.h
+++ src/placo/problem/expression.h
@@ -6,29 +6,107 @@
 namespace placo
 {
 class ProblemConstraint;
+
+/**
+ * @brief An expression is a linear combination of decision variables of the form Ax + b that can
+ * be conveniently manipulated using operators
+ */
 class Expression
 {
 public:
   Expression();
   Expression(const Expression& other);
 
-  Eigen::VectorXd value(Eigen::VectorXd x) const;
-
-  static Expression from_vector(const Eigen::VectorXd& v);
-  static Expression from_double(const double& value);
-
-  // An expression is Ax + b, where x is the decision variable
+  /**
+   * @brief Expression A matrix, in Ax + b
+   */
   Eigen::MatrixXd A = Eigen::MatrixXd(0, 0);
+
+  /**
+   * @brief Expression b vector, in Ax + b
+   */
   Eigen::VectorXd b = Eigen::VectorXd(0);
 
+  /**
+   * @brief Slice rows from a given expression
+   * @param start start row
+   * @param rows number of rows (default: -1, all rows)
+   * @return a sliced expression
+   */
   Expression slice(int start, int rows = -1) const;
 
+  /**
+   * @brief Builds an expression from a vector (A will be zeros)
+   * @param v vector
+   * @return expression
+   */
+  static Expression from_vector(const Eigen::VectorXd& v);
+
+  /**
+   * @brief Builds an expression from a double (A will be zero, the expression is only one row)
+   * @param value value
+   * @return expression
+   */
+  static Expression from_double(const double& value);
+
+  /**
+   * @brief Retrieve the expression value, given a decision variable. This can be used after a problem is
+   * solved to retrieve a specific expression value.
+   *
+   * For exemple, passing ``problem.x`` after calling \ref Problem::solve will return the value of the expression
+   * ```cpp
+   * // Value of the expression, given the problem solution
+   * expr.value(problem.x);
+   * ```
+   *
+   * @param x
+   * @return
+   */
+  Eigen::VectorXd value(Eigen::VectorXd x) const;
+
+  /**
+   * @brief checks if the expression is a scalar
+   * @return true if the expression is a scalar
+   */
   bool is_scalar() const;
 
+  /**
+   * @brief Number of cols in A
+   * @return number of cols in A
+   */
   int cols() const;
+
+  /**
+   * @brief Number of rows in A
+   * @return number of rows in A
+   */
   int rows() const;
 
+  /**
+   * @brief Adds the expression element by element to another expression
+   * @param f
+   * @return
+   */
   Expression piecewise_add(double f) const;
+
+  /**
+   * @brief Multiply an expression on the left by a given matrix M
+   * @param M matrix
+   * @return expression
+   */
+  Expression multiply(const Eigen::MatrixXd M);
+
+  /**
+   * @brief Reduces a multi-rows expression to the sum of its items
+   * @return expression
+   */
+  Expression sum();
+
+  /**
+   * @brief Reduces a multi-rows expression to the mean of its items
+   * @return expression
+   */
+  Expression mean();
 
   // Summing expressions
   Expression operator+(const Expression& other) const;
@@ -53,11 +131,6 @@ public:
 
   // Multiplying by a matrix
   friend Expression operator*(const Eigen::MatrixXd M, const Expression& e);
-  Expression multiply(const Eigen::MatrixXd M);
-
-  // Sum of all stacked expressions
-  Expression sum();
-  Expression mean();
 
   // Stacking expressions
   Expression operator/(const Expression& other) const;
