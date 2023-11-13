7636dd1fa7a4526698b81b36f686fe77e1f871fc
Grégoire Passault <g.passault@gmail.com>
2023-11-13 22:47:19 +0100

    Document __init__ methods

--- docs/conf.py
+++ docs/conf.py
@@ -18,14 +18,14 @@ extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 
 html_static_path = ['_static']
+
+autoclass_content = 'both'
 
 import os
 import sys
