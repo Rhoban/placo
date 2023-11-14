d67172ed188b26f186e3d95014e8895e1811e2d4
Grégoire Passault <g.passault@gmail.com>
2023-11-14 01:12:42 +0100

    Removing useless ext.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Rhoban Team'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx_wagtail_theme']
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
