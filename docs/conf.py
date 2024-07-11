80eef742a67c8b114ccf716df0757d3a912fc1d0
Grégoire Passault <g.passault@gmail.com>
2024-07-11 19:33:00 +0200

    Year update

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'PlaCo'
-copyright = '2023, Rhoban Team'
+copyright = '2024, Rhoban Team'
 author = 'Rhoban Team'
 
 # -- General configuration ---------------------------------------------------
