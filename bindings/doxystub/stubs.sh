83726a100df226b3475812dfef05208f6146effa
Grégoire Passault <g.passault@gmail.com>
2025-04-02 01:17:32 +0200

    Doxystub is now external

--- bindings/doxystub/stubs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INTERPRETER=$1
-MODULE_DIRECTORY=$2
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-export PYTHONPATH="$MODULE_DIRECTORY:$PYTHONPATH"
-$INTERPRETER $SCRIPT_DIR/stubs.py
